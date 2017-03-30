de95dbf2a99d5182daa551888c05ac00039929d3
GuanyaoXie <guanyaoxie@gmail.com>
2017-03-30 16:11:13 +0200

    Add files via upload

--- element_page/footer.php
+++ element_page/footer.php
@@ -1,3 +1,4 @@
 <footer>
-  <a href="https://github.com/GabrielNOIRET/Projet_Kanban">Lien Github</a>
+    <!-- lien vers le github de notre travail--> 
+  <a id="lien_github" href="https://github.com/GabrielNOIRET/Projet_Kanban">Lien Github</a>
 </footer>
