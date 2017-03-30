1a67a9f85ea62fdfd0915e00d3d37791ad64d816
GuanyaoXie <guanyaoxie@gmail.com>
2017-03-30 09:50:41 +0200

    Add files via upload

---
+++ page_projet/index.js
@@ -0,0 +1,32 @@
+var ListProjet=document.getElementById("ListProjet")
+var ListUtilisateur=document.getElementById("ListUtilisateur")
+var tableauprojet=document.getElementById("tableauprojet");
+
+
+
+var firstlist=document.getElementById("firstlist");
+
+
+document.getElementById("button").onclick=function(){
+        firstlist.style.display="block";
+        /*
+        ListProjet.innerText="Liste Projets"  
+        var thead=document.createElement("BUTTON");
+        tableauprojet.appendChild(thead);
+        tableauprojet.border=2;
+        tableauprojet.width=200;
+        tableauprojet.style.marginLeft="10px";
+    
+        for(var i=0;i<listp.length;i++){
+            var test=document.createElement("p");
+            var test2=document.createTextNode(listp[i]);
+            test.appendChild(test2);
+            tableauprojet.appendChild(test);
+        }
+            document.body.appendChild(tableauprojet);
+        
+        thead.onclick=function(){
+            ListUtilisateur.innerText="Liste Utilisateur";
+        }
+        */
+}
