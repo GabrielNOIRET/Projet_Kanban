8bcabc281a8d822f6821075a2e6cdebda49516f8
GuanyaoXie <guanyaoxie@gmail.com>
2017-03-30 14:14:31 +0200

    Delete acceuil.php

--- element_page/acceuil.php
+++
@@ -1,9 +0,0 @@
-<section>
-  <div id="bienvenue">
-    <p>Bienvenue</p>
-  </div>
-  <div id="boutons">
-    <a class="bouton" href="inscription.php">Inscription</a>
-    <a class="bouton" href="connexion.php">Connexion</a>
-  </div>
-</section>
