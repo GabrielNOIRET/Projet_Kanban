2eea690aaae0a170b880e14a7b8e754ed07d411b
Gabriel <gabriel.noiret@gmail.com>
2017-04-01 14:32:32 +0200

    Effet image font

--- pages_web/page_index.php
+++ pages_web/page_index.php
@@ -23,10 +23,25 @@
         <a class="bouton" href="page_inscription.php">Inscription</a>
           <!--lien vers la page de connection-->
         <a class="bouton" href="page_connexion.php">Connexion</a>
+
       </div>
     </div>
 
    <!-- ajouter footer : lien vers le github de notre travail-->
     <?php include 'element_page/footer.php'; ?>
+        <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+      <script>$(document).ready(function() {
+        var movementStrength = 55;
+        var height = movementStrength / $(window).height();
+        var width = movementStrength / $(window).width();
+        $("#section_img").mousemove(function(e){
+          var pageX = e.pageX - ($(window).width() / 2);
+          var pageY = e.pageY - ($(window).height() / 2);
+          var newvalueX = width * pageX * -1 - 55;
+          var newvalueY = height * pageY * -1 - 50;
+          $('#section_img').css("background-position", newvalueX+"px     "+newvalueY+"px");
+    });
+    });
+    </script>
   </body>
 </html>
