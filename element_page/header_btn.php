c875410e09482a71082dd4b18f3ef38f3832b0e3
Gabriel <gabriel.noiret@gmail.com>
2017-04-05 09:54:06 +0200

    Delete header_btn.php

--- element_page/header_btn.php
+++
@@ -1,9 +0,0 @@
-<header>
-    <!-- ajouter un button en haut à gauch de la page pour revenir à la page d'accueil--> 
-    <a id="topbtn" href="page_index.php">Revenir à la page d'accueil</a>
-    
-    <!-- titre--> 
-  <h1>Module de gestion de projet</h1>
-</header>
-<!--ligne verte--> 
-<div id="bar_verte"></div>
